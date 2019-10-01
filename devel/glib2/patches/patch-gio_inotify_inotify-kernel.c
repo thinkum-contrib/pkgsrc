61faaeeb91db8af14200b84e189dab05edc70bdf
Sean Champ <spchamp@users.noreply.github.com>
2019-09-30 22:28:40 -0700

    devel/glib2: Update pkgbase and Makefile.common to 2.62.0
    Pursuant towards updating graphics/cairo to add an OpenGL build options
    along with some corresponding build options (e.g Accelerated 2D Canvas)
    for webkit-gtk, this changeset represents one part of an effort to
    update GTK+3 and its dependencies as comprising GNOME projeccts.
    Ideally, this series of updates may serve to address some concerns with
    regards to gtk-webkit builds being produced from a newer upstream source
    release.
    
    Assuming that any one or more depenencies of webkit-gtk may serve to
    provide a definition for some missing symbols in the build, this series
    of changesets will endeavor to provide updates for glib2, gobject-
    introspection, gdk-pixbuf, atk, and lastly GTK+ packages such that have
    published a newer source version - juxtaposed to the present local
    revision for each of these components.
    
    Should any of these changesets ultimately conflict with any changesets
    published at NetBSD pkgsrc, of course the changess published by NetBSD
    pkgsrc would be assumed to take precedence to those developed here.
    
    devel/glib2
    
    - Update Makefile.common to minor version 2.62
    
    - Update PLIST for added files and updated dynobj. verions
    
    - Update meson.build patches for minor changes in upstream syntax and
      semantics
    
    - Update source level patches - this, in an effort to retain an
      effective set union of changes introduced in (A) the patches and (B)
      any changes now, in effect, published from upstream
    
      - Removed patch for glib/ghash.c - the source code has been updated
        at upstream, in a manner that may appear to represent a significant
        change to the original, unpatched source code. Hopefuly this new
        version will test out OK in all affected systems.
    
      - The patch for gio/inotify/inotify-kernel.c has been, in effect,
        trimmed for the introducton of some newer source declarations in
        the upstream source code
    
    TBD: Any pkg builds using devel/glib2 Makefile.common will need to be
    updated, subsequently, i.e
    - devel/gdbus-codegen
    - devel/glib2-tools

--- devel/glib2/patches/patch-gio_inotify_inotify-kernel.c
+++ devel/glib2/patches/patch-gio_inotify_inotify-kernel.c
@@ -5,17 +5,9 @@ Use _XOPEN_NAME_MAX in lieu of NAME_MAX for SunOS.
 Include <sys/filio.h> for FIONREAD on SunOS.
 https://gitlab.gnome.org/GNOME/glib/merge_requests/889
 
---- gio/inotify/inotify-kernel.c.orig	2016-02-23 22:25:36.000000000 +0000
+--- gio/inotify/inotify-kernel.c.orig	2019-09-05 16:56:48.000000000 +0000
 +++ gio/inotify/inotify-kernel.c
-@@ -30,11 +30,17 @@
- #include <glib.h>
- #include "inotify-kernel.h"
- #include <sys/inotify.h>
-+#ifdef HAVE_SYS_FILIO_H
-+#include <sys/filio.h>
-+#endif
- #include <glib/glib-unix.h>
- 
+@@ -38,6 +38,9 @@
  #include "glib-private.h"
  
  /* From inotify(7) */
