2c52db09d8601485b350a0c962ac1dd4d9df33b0
Sean Champ <spchamp@users.noreply.github.com>
2019-11-01 01:12:21 -0700

    print/fig2dev/: Update for source release 3.2.7b
    - print/fig2dev/Makefile: Update for the latest fig2dev release; Reuse
      rgb.txt from x11/rgb - no longer provided by fig2dev
    
    - print/fig2dev/PLIST: Remove bitmap files from PLIST - now beign
      provided in fig2dev itself; Add newer i18n files; remove rgb.txt from
      PLIST
    
    - print/fig2dev/distinfo: Updtae for latest fig2dev release and updated
      patches
    
    - print/fig2dev/patches/patch-fig2dev_fig2dev.c: Update patch for
      applying to the latest fig2dev.c

--- print/fig2dev/patches/patch-fig2dev_fig2dev.c
+++ print/fig2dev/patches/patch-fig2dev_fig2dev.c
@@ -3,15 +3,15 @@ $NetBSD: patch-fig2dev_fig2dev.c,v 1.2 2017/10/04 12:01:43 wiz Exp $
 NetBSD defines _setmode, but is not Windows.
 https://sourceforge.net/p/mcj/tickets/17/
 
---- fig2dev/fig2dev.c.orig	2017-01-04 11:04:09.000000000 +0000
+--- fig2dev/fig2dev.c.orig	2019-09-17 20:57:04.000000000 +0000
 +++ fig2dev/fig2dev.c
-@@ -37,6 +37,9 @@
+@@ -30,6 +30,9 @@
+ #include <stdarg.h>
  #include <string.h>
- #include "bool.h"
  #include <locale.h>
 +#ifdef __NetBSD__
 +#undef HAVE__SETMODE
 +#endif
- /* setmode() exists on DOS/Windows. It sets file mode to text or binary.
-  * setmode() is declared in <io.h>, O_BINARY is declared in <fcntl.h>. */
+ /* In Windows, _setmode() is declared in <io.h>, O_BINARY in <fcntl.h>. It
+  * accepts two arguments and sets file mode to text or binary. */
  #ifdef HAVE__SETMODE
