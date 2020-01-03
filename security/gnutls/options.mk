f0ad935df4aae995ce14faaf69fc341bda578989
Sean Champ <spchamp@users.noreply.github.com>
2020-01-03 13:53:07 -0800

    security/gnutls: Add explicit locale support (ported from devel/gtexinfo)

--- security/gnutls/options.mk
+++ security/gnutls/options.mk
@@ -1,7 +1,9 @@
 # $NetBSD: options.mk,v 1.2 2019/10/04 17:25:53 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnutls
-PKG_SUPPORTED_OPTIONS=	dane
+PKG_SUPPORTED_OPTIONS=	dane nls
+PKG_SUGGESTED_OPTIONS=		# blank
+PKG_SUGGESTED_OPTIONS.NetBSD+=	nls
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +13,12 @@ CONFIGURE_ARGS+=	--enable-libdane
 PLIST_SRC+=		PLIST.dane
 .else
 CONFIGURE_ARGS+=	--disable-libdane
+.endif
+
+.if !empty(PKG_OPTIONS:Mnls)
+USE_PKGLOCALEDIR=	yes
+PLIST_SRC+=		${PKGDIR}/PLIST.locale
+.  include "../../devel/gettext-lib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-nls
 .endif
