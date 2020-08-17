667d885bbfb617264adf5067652feffd8d06fe04
Sean Champ <spchamp@users.noreply.github.com>
2020-08-17 15:32:01 -0700

    devel/apr: more hacking w/ jlibtool; merge (cherry pick) local change from alternate feature branch Original changelog message:
    devel/apr: Add a formal jlibtool build option
    
    - Move the jlibtool subst into a conditional eval in options.mk
    
    - Add a new build option, jlibtool
    
    - Ensure that the bytecode jlibtool from devel/apr will not be
      overwritten previous to the build, and will not be overwritten during
      install, when the pkg is built with the jlibtool build option
    
    - Update patch and subst strings for setting the SHELL path in
      jlibtool.c constants, in addition to setting the AR and RANLIB paths
      at time of build
    
    This port may now be built as to provide a jlibtool installation,
    without using packages under wip. Albeit, this jlibtool itself may be
    somewhat more out-of-date than that available in wip/jlibtool
    
    When this pkg is built without the jlibtool option (the default), the
    file libexec/apr/libtool will nonetheless be available under LOCALBASE,
    albeit as - in effect - providing a copy of the libtool used during the
    build.
    
    (cherry picked from commit 819650b0384cff36310eb5da49a7443dee6599a0)
    (cherry picked from commit ddf6b8a51a6aca5758b5904cf24670dd49c22c3b)

--- devel/apr/options.mk
+++ devel/apr/options.mk
@@ -1,7 +1,7 @@
 # $Thinkum$
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.apr
-PKG_SUPPORTED_OPTIONS=		inet6
+PKG_SUPPORTED_OPTIONS=		inet6 jlibtool
 PKG_SUGGESTED_OPTIONS=		inet6
 
 .include "../../mk/bsd.options.mk"
@@ -10,4 +10,28 @@ PKG_SUGGESTED_OPTIONS=		inet6
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mjlibtool)
+
+SUBST_CLASSES+= 	jlibtool
+## Assumption: This will be run after patches are applied
+SUBST_STAGE.jlibtool=	pre-configure
+SUBST_FILES.jlibtool=	build/jlibtool.c
+SUBST_SED.jlibtool=	-e 's,@SH@,${SHELL},;s,@AR@,${AR},;s,@RANLIB@,${RANLIB},'
+SUBST_MESSAGE.jlibtool= Set tool paths in jlibtool
+
+## NB: The apr configure script, itself, will build jlibtool
+## during configure, if configured with --enable-experimental-libtool
+##
+## This option will prevent the built jlibtool from being overwritten
+## with a shell script
+LIBTOOL_OVERRIDE= 	# Defined
+pre-build: 
+	${CC} ${CFLAGS} -o ${WRKSRC}/libtool ${WRKSRC}/build/jlibtool.c 
+
+.else
+post-install:
+	${RM} ${DESTDIR}${PREFIX}/libexec/apr/libtool
+	${INSTALL_SCRIPT} ${PKG_LIBTOOL} ${DESTDIR}${PREFIX}/libexec/apr/libtool
 .endif
